d55c0eb2e5d29878bff8cd383e28d81b8a3d22b3
Duzie Uche-Abba <72194611+duzzisantos@users.noreply.github.com>
2022-04-13 13:18:54 +0200

    Update config.js

--- orie-server/config/config.js
+++ orie-server/config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://ucheabba:barcelonaG22@cluster0.1s7zq.mongodb.net/orie2?retryWrites=true&w=majority"
+    url: "mongodb+srv://cluster0.1s7zq.mongodb.net/?retryWrites=true&w=majority"
 }
