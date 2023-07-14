fcabcad4505688b8c22f22abaf5605b3dbe7a30e
Duzie <duzzi4x@gmail.com>
2023-07-14 19:44:12 +0200

    updated files

--- orie-server/routes/login.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-  const userLogin = require("../controllers/signup");
-  var router = require("express").Router();
-
-  router.post("/", userLogin.findOne);
-  app.use("/userlogsin", router);
-};
--- orie-server/routes/user.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-  const userSignsUp = require("../controllers/signup");
-  var router = require("express").Router();
-
-  router.post("/", userSignsUp.create);
-  app.use("/usersignsup", router);
-};
