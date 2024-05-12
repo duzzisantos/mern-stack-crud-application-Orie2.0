14841de90223a41904c8e9e32214d7a19fa07e54
Duzie <duzzi4x@gmail.com>
2024-05-12 02:04:27 +0200

    improvement

--- src/helpers/getHost.js
+++ src/helpers/getHost.js
@@ -1,3 +1,7 @@
 export const getHost = () => {
-  return process.env.REACT_APP_SERVER;
+  if (process.env.NODE_ENV === "development") {
+    return process.env.REACT_APP_LOCAL;
+  } else if (process.env.NODE_ENV === "production") {
+    return process.env.REACT_APP_SERVER;
+  }
 };
