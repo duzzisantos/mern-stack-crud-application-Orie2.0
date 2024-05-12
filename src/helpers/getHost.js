5174b47db537b502e6392c8437fd07fcddd8fbb9
Duzie <duzzi4x@gmail.com>
2024-05-12 03:04:36 +0200

    Made correction

--- src/helpers/getHost.js
+++ src/helpers/getHost.js
@@ -1,13 +1,7 @@
 export const getHost = () => {
   if (process.env.NODE_ENV === "development") {
-    console.log(process.env.REACT_APP_LOCAL);
     return process.env.REACT_APP_LOCAL;
   } else if (process.env.NODE_ENV === "production") {
     return process.env.REACT_APP_SERVER;
   }
 };
-
-export const isLocal = process.env.NODE_ENV === "development";
-export const isProduction = process.env.NODE_ENV === "production";
-export const localHost = process.env.REACT_APP_LOCAL;
-export const server = process.env.REACT_APP_SERVER;
