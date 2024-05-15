e976e7cf3ee46273998350f2b09ef204b3c87595
Duzie <duzzi4x@gmail.com>
2024-05-15 20:53:33 +0200

    removed local host

--- src/helpers/getHost.js
+++ src/helpers/getHost.js
@@ -1,7 +1,3 @@
 export const getHost = () => {
-  if (process.env.NODE_ENV === "development") {
-    return process.env.REACT_APP_LOCAL;
-  } else if (process.env.NODE_ENV === "production") {
-    return process.env.REACT_APP_SERVER;
-  }
+  return process.env.REACT_APP_SERVER;
 };
