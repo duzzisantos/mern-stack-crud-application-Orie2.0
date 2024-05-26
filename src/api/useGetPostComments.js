d9f5c0bac3b0ddd3e1904cd9a227939a5c98a448
Duzie <duzzi4x@gmail.com>
2024-05-26 17:34:34 +0200

    comment effect reinstated

--- src/api/useGetPostComments.js
+++ src/api/useGetPostComments.js
@@ -1,38 +1,37 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { getHost } from "../helpers/getHost";
 
 const useGetPostComments = (secondParty, id, token) => {
   const [comments, setComments] = useState([]);
 
-  const getComments = useCallback(async () => {
-    try {
-      const response = await axios.get(
-        `${getHost()}/api/user-posts/comments?userEmail=${secondParty}&id=${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (response.status !== 200) {
-        throw new Error(`${response.status} ${response.statusText}`);
-      } else {
-        setComments(response.data);
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }, [secondParty, id, token]);
-
   //Fetch all comments registered
   useEffect(() => {
+    const getComments = async () => {
+      try {
+        const response = await axios.get(
+          `${getHost()}/api/user-posts/comments?userEmail=${secondParty}&id=${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (response.status !== 200) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        } else {
+          setComments(response.data);
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    };
     getComments();
-  }, [getComments]);
+  }, [secondParty, id, token]);
 
-  return { comments, refetch: getComments };
+  return { comments, setComments };
 };
 
 export default useGetPostComments;
